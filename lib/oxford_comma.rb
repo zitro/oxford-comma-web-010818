77c92a233896c2da25352ab5f7550f557110b434
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 03:44:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif array.length >= 3
-    array[-1].insert(0, "and")
+    array.insert(-1, "and")
   end
   array.join(", ")
 end
